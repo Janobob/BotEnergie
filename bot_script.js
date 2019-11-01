c6261fd20d5fc7b81675dcc60b05c0904bfe11c8
Jan Werren <jan@werren.com>
2019-11-01 22:08:18 +0100

    Create bot_script.js

---
+++ bot_script.js
@@ -0,0 +1,111 @@
+// ==UserScript==
+// @name         Energy Air Game Bot
+// @namespace    http://tampermonkey.net/
+// @version      1.1
+// @description  Beat the energy air game!
+// @author       Jan Werren
+// @match        https://game.energy.ch/
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    var speed = 2000;
+    var running = true;
+    var selectMode = ".tickets"; //".partner-price" for lipo
+
+    var answerArray = [
+        "Hallenstadion, Zürich",
+        "22. November 2019",
+        "Twitter",
+        "Swisscom",
+        "«The Game Is On»",
+        "Über 1'000'000",
+        "Mark Forster",
+        "VIP",
+        "#esn19",
+        "Möbel",
+        "Energy Star Night findet trotzdem statt",
+        "Ein Schweizer Musikpreis",
+        "Oktober 2016",
+        "Energy Star Night 2018",
+        "Usgang.ch",
+        "Graubünden",
+        "Energy Stars For Free",
+        "Die siebzehnte",
+        "Lo & Leduc",
+        "Hiltl Club, Zürich",
+        "Mex",
+        "Reinach (Kanton Basel)",
+        "gewinnen",
+        "Die tiefsten Preise der Schweiz",
+        "Orange",
+        "Jastina Doreen, Ex-Miss Schweiz",
+        "Twitter",
+        "Stefanie Heinzmann",
+        "14'000",
+        "Winterthur und Villeneuve",
+        "2",
+        "Über 1'000'000",
+        "dem Schweizerischen Roten Kreuz",
+        "Hallenstadion, Zürich",
+        "Vorhänge individuell konfigurieren",
+        "2003",
+        "22. November 2019",
+        "E-Mail",
+        "Um 19:00 Uhr",
+        "im Radio, auf der Event-Website und über Social Media",
+        "22",
+        "das zweitgrösste Musik-Event der Schweiz!"
+    ];
+
+    function selectRightAnswer(){
+        answerArray.forEach(function(answer) {
+            if($(document.getElementById(answer)).length == 1){
+                $(document.getElementById(answer)).prop("checked", true).trigger("click");
+            }
+        });
+    }
+
+    function sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
+
+    async function play(){
+        running = true;
+        while(running){
+            while($("#next-question").length > 0){
+                selectRightAnswer();
+                await sleep(speed);
+                $("#next-question").trigger("click");
+                await sleep(speed);
+            }
+            await sleep(speed);
+            $(selectMode).trigger("click");
+            await sleep(speed);
+            var circles = document.getElementsByClassName("circle");
+            $($(circles)[getRandomInt($(circles).length)]).find("img").trigger("click");
+            await sleep(speed);
+            $(".pulse").trigger("click");
+            await sleep(speed);
+            if($("#lose").length == 1){
+                $("#lose").trigger("click");
+            }
+            if($(".game-button").length == 1){
+                $(".game-button").trigger("click");
+            }
+            await sleep(speed);
+        }
+    }
+
+    function stop(){
+        running = false;
+    }
+
+    function getRandomInt(max) {
+        return Math.floor(Math.random() * Math.floor(max));
+    }
+    
+play();
+})();
